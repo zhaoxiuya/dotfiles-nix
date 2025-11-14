026227f35dd7e3391021dca346869a79ef4bb5b3
tmp <tmp@local>
2025-11-14 21:07:44 +0900

    aa

--- flake.nix
+++ flake.nix
@@ -1,25 +1,30 @@
 {
-  description = "NixOS  system of Zhaoxiuya";
+  description = "NixOS system of Zhaoxiuya";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";  
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/labtop
-        ./modules
-      ];
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.zhaoxiuya = import ./home/zhaoxiuya/home.nix;
-      }
+  outputs = { self, nixpkgs, home-manager }:
+    {
+      nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/labtop
+          ./modules
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.zhaoxiuya = import ./home/zhaoxiuya/home.nix;
+          }
+        ];
+      };
     };
-  };
 }
+
