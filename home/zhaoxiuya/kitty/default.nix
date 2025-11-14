c4c033f75ffd7acd0690064380756db4392691ea
tmp <tmp@local>
2025-11-14 13:13:56 +0000

    isisadasdi

--- home/zhaoxiuya/kitty/default.nix
+++ home/zhaoxiuya/kitty/default.nix
@@ -1,14 +1,17 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
-  enable = true;
-  
-  font = {
-    name = "Monoid";
-    size = 13;
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "Monoid";
+      size = 13;
+    };
+
+    extraConfig = ''
+      include ${pkgs.kitty-themes}/share/kitty-themes/themes/gruvbox_dark.conf
+    '';
   };
-  
-  extraConfig = ''
-    include ${pkgs.kitty-themes}/share/kitty-themes/gruvbox_dark.conf
-  '';
 }
+
