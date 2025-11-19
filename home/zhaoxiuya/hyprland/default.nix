a05885c8b65aadc2b52b7810a4d0cdccc82e3c54
tmp <tmp@local>
2025-11-19 16:15:26 +0900

    hypr

--- home/zhaoxiuya/hyprland/default.nix
+++ home/zhaoxiuya/hyprland/default.nix
@@ -11,6 +11,16 @@
         "SUPER, F, fullscreen"
         "SUPER SHIFT, R, exec, hyprctl reload"
       ];
+
+      decoration = {
+        rounding = 10;
+      };
+
+      general = {
+        border_size = 3;
+        "col.active_border" = "rgba(88ccff 1.0)";
+        "col.inactive_border" = "rgba(444444 0.7)";
+      };
     };
   };
 }
