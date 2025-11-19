b54d332e2e01395c914635f3af3addbe95a4641c
tmp <tmp@local>
2025-11-19 22:38:15 +0900

    fixed git settings

--- home/zhaoxiuya/git/default.nix
+++ home/zhaoxiuya/git/default.nix
@@ -4,10 +4,12 @@
   programs.git = {
     enable = true;
 
-    userName = "Sua Tso";
-    userEmail = "zhaoxiuya13@gmail.com";
+    #userName = "Sua Tso";
+    #userEmail = "zhaoxiuya13@gmail.com";
 
     settings.user = {
+      name = "Sua Tso";
+      email = "zhaoxiuya13@gmail.com";
       init.defaultBranch = "main";
       credential.helper = "store";
     };
