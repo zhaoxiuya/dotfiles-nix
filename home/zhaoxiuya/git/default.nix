a58cef949c4179572aebdddd5839d3ffce99802e
zhaoxiuya <zhaoxiuya@local>
2025-11-19 22:46:38 +0900

    fixed git

--- home/zhaoxiuya/git/default.nix
+++ home/zhaoxiuya/git/default.nix
@@ -7,11 +7,14 @@
     #userName = "Sua Tso";
     #userEmail = "zhaoxiuya13@gmail.com";
 
-    settings.user = {
-      name = "Sua Tso";
-      email = "zhaoxiuya13@gmail.com";
+    settings = {
+      user = {
+        name = "Sua Tso";
+        email = "zhaoxiuya13@gmail.com";
+      };
       init.defaultBranch = "main";
       credential.helper = "store";
+      safe.directory = "/etc/nixos";
     };
   };
 
