e1c150203c028eb094e089982c537e30549e6c28
zhaoxiuya <zhaoxiuya@local>
2025-11-20 14:38:12 +0900

    cat

---
+++ home/zhaoxiuya/stylix/default.nix
@@ -0,0 +1,27 @@
+{
+  stylix = {
+    enable = true;
+
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/wallpapers/main/fractal/1080p/blue.png";
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+
+    base16Scheme = "catppuccin-mocha";
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts;
+        name = "JetBrainsMono Nerd Font";
+        size = 11;
+      };
+    };
+
+    targets = {
+      waybar.enable = true;
+      hyprland.enable = true;
+      kitty.enable = true;
+    };
+  };
+}
+
