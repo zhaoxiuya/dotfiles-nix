314f5caafcdcf22149d73854d8b84a8335548b32
zhaoxiuya <zhaoxiuya@local>
2025-11-20 14:45:39 +0900

    cat

--- home/zhaoxiuya/stylix/default.nix
+++ home/zhaoxiuya/stylix/default.nix
@@ -9,7 +9,7 @@
       sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
     };
 
-    base16Scheme = "catppuccin-mocha";
+    base16Scheme = "${stylix.lib.base16.schemes.catppuccin-mocha}";
 
     fonts = {
       sizes = {
